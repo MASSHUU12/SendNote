0e2bef8e5a26efc982038a4b414d91a43869a9ca
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2022-01-02 20:20:09 +0100

    Home: header and form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,100 @@
 @extends('master') @section('title') Home @endsection @section('content')
 <div class="home-container">
-    <h1>home</h1>
+    <div class="home-desc-main">
+        <h1>A simple and secure way to share notes</h1>
+        <h3>
+            SendNote is a free online service that allows you to share
+            notes/messages securely. The notes you send are encrypted and
+            deleted as quickly as possible, whether after a set period of time
+            or after a set number of views.
+        </h3>
+    </div>
+    <div class="home-content">
+        <form action="/create" method="post">
+            @csrf
+            <ul>
+                <li>
+                    <input
+                        type="text"
+                        name="title"
+                        placeholder="Note title..."
+                    />
+                </li>
+                <li>
+                    <label>
+                        Expiration date
+                        <input type="date" name="expiration_date" />
+                    </label>
+                </li>
+            </ul>
+
+            <textarea
+                name="note_content"
+                cols="30"
+                rows="10"
+                placeholder="Write a note..."
+            ></textarea>
+
+            {{-- item --}}
+            <div class="label-checkbox">
+                <label
+                    ><input
+                        type="checkbox"
+                        name="with_password"
+                        id="with_password" />
+                    <span></span></label
+                >Secure with password
+            </div>
+            <div class="label-hidden" id="label-pass">
+                <label for="password"
+                    >Enter password to secure the message</label
+                >
+                <input type="password" name="password" />
+                <label for="password_conf">Confirm password</label>
+                <input type="password" name="password_conf" />
+            </div>
+
+            {{-- item --}}
+            <div class="label-checkbox">
+                <label>
+                    <input
+                        type="checkbox"
+                        name="with_notification"
+                        id="with_notification" />
+                    <span></span></label
+                >Destruction notification
+            </div>
+            <div class="label-hidden" id="label-notif">
+                <label for="email"
+                    >Email address to notify when the message is
+                    destroyed</label
+                >
+                <input type="email" name="email" />
+                <label for="email_ref"
+                    >Reference name for the message (optional)</label
+                >
+                <input type="text" name="email_ref" />
+            </div>
+
+            {{-- item --}}
+            <div class="label-checkbox">
+                <label>
+                    <input type="checkbox" name="with_views" id="with_views" />
+                    <span></span></label
+                >Destroy after number of views
+            </div>
+            <div class="label-hidden label-hidden-alt" id="label-views">
+                <label
+                    ><input type="number" name="views" /><span
+                        >view/s</span
+                    ></label
+                >
+            </div>
+
+            <input type="submit" name="submit" value="Create message" />
+        </form>
+    </div>
 </div>
+@endsection @section('scripts')
+<script src="{{ asset('js/showOptions.js') }}"></script>
 @endsection
